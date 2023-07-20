19ca243531727e8a805d3e6e61534d40609b211a
Zadrick <zadrick.d.geron@accenture.com>
2023-07-21 01:21:44 +0800

    init dashboard

---
+++ frontend/src/components/MainDashboard/MainDashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MainDashboard extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div>HELLO WORLD</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MainDashboard;
